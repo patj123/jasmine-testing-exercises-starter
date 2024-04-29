69fe9d04d73581c6925da0a6b42bae8c25d3247d
Patrick Gibbs <patrickjkgibbs@gmail.com>
2024-04-29 15:30:25 -0700

    monthly rate variable added

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -24,7 +24,7 @@ function setupIntialValues() {
   const loanAmount = document.getElementById('loan-amount');
   const loanYears = document.getElementById('loan-years');
   const loanRate = document.getElementById('loan-rate');
-  const initialValues = {amount: 100000, years: 3, rate: 5};
+  const initialValues = {amount: 100000, years: 3, rate: 5.00};
   loanAmount.value = initialValues.amount;
   loanYears.value = initialValues.years;
   loanRate.value = initialValues.rate;
@@ -34,16 +34,19 @@ function setupIntialValues() {
 // Update the monthly payment
 function update() {
   const initialUIValues = getCurrentUIValues();
-  
+  calculateMonthlyPayment(initialUIValues);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate/100)/12
+  
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+
 }
