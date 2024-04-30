3b93ed7a79eae047816039afd8ad16684095a2af
Patrick Gibbs <patrickjkgibbs@gmail.com>
2024-04-30 16:25:33 -0700

    servers.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -10,9 +10,29 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
+  
+  it('should not add a new server on submitServerInfo() with empty inputs', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should update #serverTable on updateServerTable()', function () {
+
+    submitServerInfo();
+    updateServerTable();
+
+    let currentTDList = document.querySelectorAll('#serverTable tbody tr td');
+    expect(currentTDList.length).toEqual(2);
+    expect(currentTDList[0].innerText).toEqual('Alice');
+    expect(currentTDList[1].innerText).toEqual('$0.00');
+  });
 
   afterEach(function() {
     // teardown logic
-    
+    serverId = 0;
+    allServers = {};
+    serverTbody.innerHTML = '';
   });
 });
